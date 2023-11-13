40c1faacb939b562d5a6c6a2e0de3221b0c7308f
shaik faizan <shaikfaizan@shaiks-MacBook-Air.local>
2023-11-13 13:13:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Nav from "./Components/Nav";
+import Home from "./Components/Home";
+import Project from "./Components/Project";
+import Skills from "./Components/skills";
+// import Cards from "./components/cards";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Home />
+      {/* <Cards/> */}
+      <Skills />
+      <Project />
     </div>
   );
 }
